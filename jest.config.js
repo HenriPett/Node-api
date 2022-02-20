326be6cac8f97d3b10c814dcdcf7a12edcfaca29
Henrique <henriquepett@gmail.com>
2022-02-20 10:39:16 -0300

    upd: test coverage upd (100% coverage)

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
 
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'], // ira testar todo arquivo dentro de src e que seja .ts
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**',
+  ], // ira testar todo arquivo dentro de src e que seja .ts
 
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
 
   testEnvironment: 'node',
 

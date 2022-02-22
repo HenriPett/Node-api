7794834eee5e8f6f327811733027fd4e8aff11ad
Henrique <henriquepett@gmail.com>
2022-02-22 07:52:09 -0300

    upd: email validator test

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,29 @@
 import { EmailValidatorAdapter } from './email-validator'
 
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
 describe('EmailValidator adapter', () => {
   test('Should return false if validator return false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@')
+    jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator return true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid@gmail.com')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(sut, 'isValid')
+    sut.isValid('any@gmail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any@gmail.com')
   })
 })

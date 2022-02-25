7b6d48345545d688c31a26d3140b0bcb09df84ac
Henrique <henriquepett@gmail.com>
2022-02-25 07:01:41 -0300

    add: bcrypt test that ensure returned hash password

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -3,7 +3,7 @@ import { Encrypter } from '../../data/protocols/encrypter'
 
 export class BcryptAdapter implements Encrypter {
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, 12)
-    return null
+    const hash = await bcrypt.hash(value, 12)
+    return hash
   }
 }

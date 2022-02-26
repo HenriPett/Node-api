aeb824563bdff63f527a959e8dfea61cd0112208
Henrique <henriquepett@gmail.com>
2022-02-26 08:29:13 -0300

    add: test ensure that account is inserted at db

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = await new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'name',
+      email: 'email@mail.com',
+      password: 'password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('name')
+    expect(account.email).toBe('email@mail.com')
+    expect(account.password).toBe('password')
+  })
+})

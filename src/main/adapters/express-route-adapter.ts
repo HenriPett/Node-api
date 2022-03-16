ced05825aa31ec2a8ae572f931cda26880c3a203
Henrique <henriquepett@gmail.com>
2022-03-16 06:49:00 -0300

    ref: variable name

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -2,15 +2,15 @@ import { Controller, HttpRequest } from '../../presentation/protocols'
 import { Request, Response } from 'express'
 
 export const adaptRoute = (controller: Controller) => {
-  return async (req: Request, res: Response) => {
+  return async (req: Request, resp: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body
     }
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
-      res.status(httpResponse.statusCode).json(httpResponse.body)
+      resp.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
-      res
+      resp
         .status(httpResponse.statusCode)
         .json({ error: httpResponse.body.message })
     }
